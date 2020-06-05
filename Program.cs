ae5a539050ba7de603fa596555ac5ed7c46f0ba8
Naimul islam <50922314+naimul3070@users.noreply.github.com>
2020-06-06 02:07:49 +0600

    simple marksheet grading system by C#
    Here I make a grading system program by using if else condition In C#

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Greading_system
+{
+
+    enum title
+    {
+        Mr,
+        Dr,
+        Mrs,
+        Ms
+    }
+    class Students
+    {
+        public int Id;
+        public string Name;
+        public string Contact;
+    }
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            int mark = 100;
+            if (mark > 100)
+            {
+                Console.WriteLine("Please input the valid mark");
+            }
+            else
+            {
+                if(mark >= 0)
+                {
+                    Console.WriteLine("Please input the valid mark");
+                }
+                else
+                {
+                    if (mark >= 80)
+                    {
+                        Console.WriteLine("A+");
+                    }
+                    else
+                    {
+                        if (mark >= 70)
+                        {
+                            Console.WriteLine("A");
+                        }
+                        else
+                        {
+                            if (mark >= 60)
+                            {
+                                Console.WriteLine("B+");
+                            }
+                            else
+                            {
+                                if (mark >= 50)
+                                {
+                                    Console.WriteLine("B");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("F");
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
